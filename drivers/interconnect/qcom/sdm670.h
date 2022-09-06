a8a54ed34f6a7868df6000dcc5b569075e5c7c46
Richard Acayan <mailingradian@gmail.com>
2022-09-05 22:04:12 -0400

    interconnect: qcom: add sdm670 interconnects
    The interconnects for Snapdragon 670 can be controlled via RPMh. Add
    driver support for this.
    
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>

---
+++ drivers/interconnect/qcom/sdm670.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (c) 2022, Richard Acayan. All rights reserved.
+ */
+
+#ifndef __DRIVERS_INTERCONNECT_QCOM_SDM670_H__
+#define __DRIVERS_INTERCONNECT_QCOM_SDM670_H__
+
+#define SDM670_MASTER_EMMC				133
+#define SDM670_SLAVE_EMMC_CFG				134
+
+#endif /*  __DRIVERS_INTERCONNECT_QCOM_SDM670_H__ */
