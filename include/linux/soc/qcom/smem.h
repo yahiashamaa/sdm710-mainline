3c86d14bc0a02ab140e5140c0f18a8aa4dac91b1
Richard Acayan <mailingradian@gmail.com>
2022-12-05 18:09:43 -0500

    soc: qcom: smem: expose socinfo platform device
    The modemsmem driver needs detailed information about the SoC. Expose
    the socinfo platform device so other drivers can fetch SoC information.
    
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>

--- include/linux/soc/qcom/smem.h
+++ include/linux/soc/qcom/smem.h
@@ -2,7 +2,12 @@
 #ifndef __QCOM_SMEM_H__
 #define __QCOM_SMEM_H__
 
+#include <linux/err.h>
+#include <linux/platform_device.h>
+
 #define QCOM_SMEM_HOST_ANY -1
+
+struct qcom_socinfo;
 
 int qcom_smem_alloc(unsigned host, unsigned item, size_t size);
 void *qcom_smem_get(unsigned host, unsigned item, size_t *size);
@@ -10,5 +15,14 @@ void *qcom_smem_get(unsigned host, unsigned item, size_t *size);
 int qcom_smem_get_free_space(unsigned host);
 
 phys_addr_t qcom_smem_virt_to_phys(void *p);
+
+#ifdef CONFIG_QCOM_SOCINFO
+struct platform_device *qcom_smem_get_socinfo(void);
+#else
+static inline struct platform_device *qcom_smem_get_socinfo(void)
+{
+	return ERR_PTR(-ENOSYS);
+}
+#endif
 
 #endif
