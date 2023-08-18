65fc0ae495af62415dfc24ebcbaf61b347fbe77d
Richard Acayan <mailingradian@gmail.com>
2023-08-18 19:24:25 -0400

    ASoC: qcom: sdm660-internal: add suspend and headphone support
    Things were done so differently from the SDM845 sound card that multiple
    ports and a shutdown function could not be supported. Fix these issues,
    populate the shutdown function, handle the internal MI2S port for
    headset playback, and add a headphone jack.
    
    Also depend on CONFIG_OF and CONFIG_PM as the driver is not
    compile-tested without them.
    
    Amends: ASoC: qcom: add sdm660 internal sound card support
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>

--- sound/soc/qcom/sdm660-internal.c
+++ sound/soc/qcom/sdm660-internal.c
@@ -9,53 +9,82 @@
 #include <linux/platform_device.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
+#include <sound/jack.h>
 #include <sound/soc.h>
+#include <sound/soc-card.h>
 #include <sound/soc-dai.h>
 #include <sound/soc-dapm.h>
+#include <sound/soc-jack.h>
 
 #include "common.h"
 #include "qdsp6/q6afe.h"
 
 #define DEFAULT_SAMPLE_RATE_48K		48000
+#define DEFAULT_INT_MCLK_RATE		9600000
 #define TDM_BCLK_RATE			6144000
+#define MI2S_BCLK_RATE			1536000
+
+struct sdm660_int_snd_data {
+	struct snd_soc_jack jack;
+	bool jack_setup;
+	uint32_t sec_tdm_clk_count;
+	uint32_t int0_mi2s_clk_count;
+	uint32_t int_mclk0_count;
+};
 
 static int snd_sdm660_int_startup(struct snd_pcm_substream *stream)
 {
 	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(stream);
+	struct sdm660_int_snd_data *data = snd_soc_card_get_drvdata(rtd->card);
+	struct snd_soc_dai *cpu = asoc_rtd_to_cpu(rtd, 0);
 	struct snd_soc_dai *codec;
-	struct snd_soc_dai *cpu;
-	int i, ret;
+	int i;
 
-	for_each_rtd_cpu_dais(rtd, i, cpu)
-		snd_soc_dai_set_sysclk(cpu,
-			Q6AFE_LPASS_CLK_ID_SEC_TDM_IBIT,
-			TDM_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
+	switch (cpu->id) {
+	case SECONDARY_TDM_RX_0:
+		data->sec_tdm_clk_count++;
+		if (data->sec_tdm_clk_count)
+			snd_soc_dai_set_sysclk(cpu,
+				Q6AFE_LPASS_CLK_ID_SEC_TDM_IBIT,
+				TDM_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
 
-	// Only use this for TDM.
-	for_each_rtd_codec_dais(rtd, i, codec) {
-		ret = snd_soc_dai_set_fmt(codec, SND_SOC_DAIFMT_DSP_A
-					       | SND_SOC_DAIFMT_CBC_CFC
-					       | SND_SOC_DAIFMT_IB_NF);
-		if (ret != 0) {
-			dev_err(codec->dev, "set dai format failed\n");
-			return ret;
+		for_each_rtd_codec_dais(rtd, i, codec) {
+			snd_soc_dai_set_fmt(codec, SND_SOC_DAIFMT_DSP_A
+						 | SND_SOC_DAIFMT_CBC_CFC
+						 | SND_SOC_DAIFMT_IB_NF);
+
+			snd_soc_dai_set_sysclk(codec, 0,
+					       TDM_BCLK_RATE,
+					       SNDRV_PCM_STREAM_PLAYBACK);
+			snd_soc_component_set_sysclk(codec->component,
+						     0, 0,
+						     TDM_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
 		}
+		break;
+	case INT0_MI2S_RX:
+		data->int0_mi2s_clk_count++;
+		if (data->int0_mi2s_clk_count)
+			snd_soc_dai_set_sysclk(cpu,
+				Q6AFE_LPASS_CLK_ID_INT0_MI2S_IBIT,
+				MI2S_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
 
-		ret = snd_soc_dai_set_sysclk(codec, 0,
-					     TDM_BCLK_RATE,
-					     SNDRV_PCM_STREAM_PLAYBACK);
-		if (ret != 0) {
-			dev_err(codec->dev, "set dai sysclk failed\n");
-			return ret;
-		}
+		data->int_mclk0_count++;
+		if (data->int_mclk0_count)
+			snd_soc_dai_set_sysclk(cpu,
+				Q6AFE_LPASS_CLK_ID_INT_MCLK_0,
+				DEFAULT_INT_MCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
 
-		ret = snd_soc_component_set_sysclk(codec->component,
-						   0, 0,
-						   TDM_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
-		if (ret != 0) {
-			dev_err(codec->dev, "set dai sysclk failed\n");
-			return ret;
-		}
+		/*
+		 * Downstream specifies that the AFE is a clock consumer, but
+		 * the sound is distorted (loud on the right channel and sped
+		 * up) unless we set it as a producer.
+		 */
+		snd_soc_dai_set_fmt(cpu, SND_SOC_DAIFMT_CBP_CFP);
+
+		break;
+	default:
+		dev_err(cpu->dev, "unimplemented afe dai\n");
+		return -ENOSYS;
 	}
 
 	return 0;
@@ -63,6 +92,37 @@ static int snd_sdm660_int_startup(struct snd_pcm_substream *stream)
 
 static void snd_sdm660_int_shutdown(struct snd_pcm_substream *stream)
 {
+	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(stream);
+	struct sdm660_int_snd_data *data = snd_soc_card_get_drvdata(rtd->card);
+	struct snd_soc_dai *cpu = asoc_rtd_to_cpu(rtd, 0);
+
+	switch (cpu->id) {
+	case SECONDARY_TDM_RX_0:
+		data->sec_tdm_clk_count--;
+		if (!data->sec_tdm_clk_count)
+			snd_soc_dai_set_sysclk(cpu,
+				Q6AFE_LPASS_CLK_ID_SEC_TDM_IBIT,
+				0, SNDRV_PCM_STREAM_PLAYBACK);
+
+		break;
+	case INT0_MI2S_RX:
+		data->int0_mi2s_clk_count--;
+		if (!data->int0_mi2s_clk_count)
+			snd_soc_dai_set_sysclk(cpu,
+				Q6AFE_LPASS_CLK_ID_INT0_MI2S_IBIT,
+				MI2S_BCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
+
+		data->int_mclk0_count--;
+		if (!data->int_mclk0_count)
+			snd_soc_dai_set_sysclk(cpu,
+				Q6AFE_LPASS_CLK_ID_INT_MCLK_0,
+				DEFAULT_INT_MCLK_RATE, SNDRV_PCM_STREAM_PLAYBACK);
+
+		break;
+	default:
+		dev_err(cpu->dev, "unimplemented afe dai\n");
+		break;
+	}
 }
 
 static unsigned int tdm_slot_off[] = {
@@ -73,11 +133,12 @@ static int snd_sdm660_int_hw_params(struct snd_pcm_substream *stream,
 				    struct snd_pcm_hw_params *params)
 {
 	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(stream);
-	struct snd_soc_dai *cpu;
+	struct snd_soc_dai *cpu = asoc_rtd_to_cpu(rtd, 0);
 	unsigned int channels;
-	int i, ret;
+	int ret;
 
-	for_each_rtd_cpu_dais(rtd, i, cpu) {
+	switch (cpu->id) {
+	case SECONDARY_TDM_RX_0:
 		channels = params_channels(params);
 
 		ret = snd_soc_dai_set_tdm_slot(cpu, 0, (1 << channels) - 1, 8, 16);
@@ -92,6 +153,11 @@ static int snd_sdm660_int_hw_params(struct snd_pcm_substream *stream,
 			dev_err(cpu->dev, "set channel map failed\n");
 			return ret;
 		}
+		break;
+	case INT0_MI2S_RX:
+		fallthrough;
+	default:
+		break;
 	}
 
 	return 0;
@@ -131,16 +197,58 @@ static int sdm660_int_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	return 0;
 }
 
-static const struct snd_soc_dapm_widget sdm845_snd_widgets[] = {
-	SND_SOC_DAPM_HP("Headphone Jack", NULL),
-	SND_SOC_DAPM_MIC("Headset Mic", NULL),
-	SND_SOC_DAPM_SPK("Left Spk", NULL),
-	SND_SOC_DAPM_SPK("Right Spk", NULL),
-	SND_SOC_DAPM_MIC("Int Mic", NULL),
-};
+static void sdm660_int_jack_free(struct snd_jack *jack)
+{
+	struct snd_soc_component *component = jack->private_data;
+
+	snd_soc_component_set_jack(component, NULL, NULL);
+}
 
 static int sdm660_int_dai_init(struct snd_soc_pcm_runtime *rtd)
 {
+	struct snd_soc_card *card = rtd->card;
+	struct sdm660_int_snd_data *data = snd_soc_card_get_drvdata(card);
+	struct snd_soc_dai *cpu = asoc_rtd_to_cpu(rtd, 0);
+	/* first codec on INT0_MI2S_RX must be the analog codec */
+	struct snd_soc_dai *codec = asoc_rtd_to_codec(rtd, 0);
+	struct snd_jack *jack;
+	int ret;
+
+	if (!data->jack_setup) {
+		/* headset buttons not tested */
+		ret = snd_soc_card_jack_new(card, "Headset Jack",
+					    SND_JACK_HEADSET | SND_JACK_BTN_0
+					  | SND_JACK_BTN_1 | SND_JACK_BTN_2
+					  | SND_JACK_BTN_3 | SND_JACK_BTN_4,
+					    &data->jack);
+		if (ret < 0) {
+			dev_err(card->dev, "could not create headset jack\n");
+			return ret;
+		}
+
+		data->jack_setup = true;
+	}
+
+	switch (cpu->id) {
+	case INT0_MI2S_RX:
+		jack = data->jack.jack;
+
+		jack->private_data = codec;
+		jack->private_free = sdm660_int_jack_free;
+
+		ret = snd_soc_component_set_jack(codec->component,
+						 &data->jack,
+						 NULL);
+		if (ret < 0) {
+			dev_err(card->dev, "could not set headset jack\n");
+			return ret;
+		}
+
+		break;
+	default:
+		break;
+	}
+
 	return 0;
 }
 
@@ -165,10 +273,15 @@ static const struct snd_soc_dapm_widget snd_sdm660_int_dapm_widgets[] = {
 static int snd_sdm660_int_probe(struct platform_device *pdev)
 {
 	struct snd_soc_card *card;
+	struct sdm660_int_snd_data *data;
 	struct device *dev = &pdev->dev;
 	int ret;
 
 	card = devm_kzalloc(dev, sizeof(struct snd_soc_card), GFP_KERNEL);
+	if (!card)
+		return -ENOMEM;
+
+	data = devm_kzalloc(dev, sizeof(struct sdm660_int_snd_data), GFP_KERNEL);
 	if (!card)
 		return -ENOMEM;
 
@@ -181,6 +294,8 @@ static int snd_sdm660_int_probe(struct platform_device *pdev)
 	ret = qcom_snd_parse_of(card);
 	if (ret)
 		return ret;
+
+	snd_soc_card_set_drvdata(card, data);
 
 	snd_sdm660_int_add_ops(card);
 
@@ -204,6 +319,7 @@ static struct platform_driver snd_sdm660_int_driver = {
 	.driver = {
 		.name = "sdm660-int-sndcard",
 		.of_match_table = snd_sdm660_int_device_id,
+		.pm = &snd_soc_pm_ops,
 	},
 };
 module_platform_driver(snd_sdm660_int_driver);
