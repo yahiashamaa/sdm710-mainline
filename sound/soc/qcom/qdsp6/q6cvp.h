5a020ed354e1b2cd783e2242cc9964249736d800
Richard Acayan <mailingradian@gmail.com>
2023-09-11 21:48:47 -0400

    ASoC: qdsp6: q6cvp: add commands in cvd 2.3
    The CVD 2.3 revision expects a different initialization sequence.
    Implement the commands to make this possible.
    
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>

--- sound/soc/qcom/qdsp6/q6cvp.h
+++ sound/soc/qcom/qdsp6/q6cvp.h
@@ -8,6 +8,11 @@ struct q6voice_session;
 
 struct q6voice_session *q6cvp_session_create(enum q6voice_path_type path,
 					     u16 tx_port, u16 rx_port);
+struct q6voice_session *q6cvp_session_create_v3(enum q6voice_path_type path,
+						u16 tx_port, u16 rx_port);
+int q6cvp_send_channel_info(struct q6voice_session *cvp, bool is_tx);
+int q6cvp_send_media_format(struct q6voice_session *cvp, int port_id, bool is_tx);
+int q6cvp_topology_commit(struct q6voice_session *cvp);
 int q6cvp_enable(struct q6voice_session *cvp, bool enable);
 
 #endif /*_Q6_CVP_H */
